469af16f56d0567569660d7ed771b22634d93751
molnard1 <101341428+molnard1@users.noreply.github.com>
2023-10-02 10:47:13 +0200

    Add more form elements

--- src/js/Form.jsx
+++ src/js/Form.jsx
@@ -1,31 +1,38 @@
 export function FormItem(props) {
+    const isRequired = props.required ? { required: true } : {};
+
+    if(props.class === undefined) props.class = "";
+
     switch(props.type) {
         case "text":
         case "password":
         case "email": {
             return [
-                <input type={props.type} name={props.name} placeholder={props.text} class="input input-bordered w-full max-w-xs" required={props.required} />
+                <input type={props.type} name={props.name} placeholder={props.text} class={`input input-bordered w-full max-w-xs ${props.class}`} {...isRequired} />
             ]
         }
 
         case "checkbox": {
             return [
-                <label class="label cursor-pointer">
-                    <span class="label-text">{props.text}</span> 
-                    <input type="checkbox" name={props.name} checked={props.checked} class="checkbox" required={props.required} />
+                <label class="label justify-normal cursor-pointer">
+                    <input type="checkbox" name={props.name} checked={props.checked} class={`checkbox ${props.class}`} {...isRequired} />
+                    <span class="label-text pl-2">{props.text}</span> 
                 </label>
             ]
         }
 
         case "radio": {
             return [
-                <input type="radio" name={props.name} class="radio" required={props.required} />
+                    <label class="label justify-normal cursor-pointer">
+                        <input type="radio" name={props.name} value={props.text} class={`radio ${props.class}`} {...isRequired} />
+                        <span class="label-text pl-2">{props.text}</span> 
+                     </label>
             ]
         }
 
         case "submit": {
             return [
-                <button type="submit" class="btn">{props.text}</button>
+                <button type="submit" class={`btn ${props.class}`}>{props.text}</button>
             ]
         }
     }
@@ -33,7 +40,7 @@ export function FormItem(props) {
 
 export function Form(props) {
     return [
-        <form class="form-control w-full max-w-xs">
+        <form class={`form-control w-full max-w-xs ${props.class}`}>
             {...props.items}
         </form>
     ]
