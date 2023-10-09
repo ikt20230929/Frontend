4652e92d934978b1b210ca8dfe87011ba1e5829c
molnard1 <101341428+molnard1@users.noreply.github.com>
2023-10-09 13:14:34 +0200

    React Hook Form continue

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -10,7 +10,6 @@ export default function App() {
   const { control, handleSubmit, setValue, formState: { errors } } = useForm();
   const onSubmit = (data) => {
     console.log(data);
-    console.log(errors);
   };
 
   return [
@@ -21,31 +20,31 @@ export default function App() {
     <Form classes="container mx-auto" onsubmit={handleSubmit(onSubmit)} items={[
       <div className="space-y-2">
         <FormItemSet classes="join flex justify-center items-center" items={[
-          <Controller name="name" control={control} render={({ field }) => [
-              <User className="ml-3" />,
-              <FormItem field={field} type="text" text="Név" />
+          <Controller rules={{ required: true }} name="name" control={control} render={({ field }) => [
+            <User className="ml-3" />,
+            <FormItem field={field} type="text" text="Név" />
           ]} />
         ]} />
 
         <FormItemSet classes="join flex justify-center items-center" items={[
-          <Controller name="profession" control={control} render={({ field }) => [
-              <Briefcase className="ml-3" />,
-              <FormItem field={field} type="text" text="Foglalkozás" required={true} />
+          <Controller rules={{ required: true }} name="profession" control={control} render={({ field }) => [
+            <Briefcase className="ml-3" />,
+            <FormItem field={field} type="text" text="Foglalkozás" />
           ]} />
         ]} />
 
 
         <FormItemSet classes="join flex justify-center items-center" items={[
-          <Controller name="email" control={control} render={({ field }) => [
-              <Mail className="ml-3" />,
-              <FormItem field={field} type="email" text="Email (.com/.hu/.net/.edu)" required={true} />
+          <Controller rules={{ required: true }} name="email" control={control} render={({ field }) => [
+            <Mail className="ml-3" />,
+            <FormItem field={field} type="email" text="Email (.com/.hu/.net/.edu)" />
           ]} />
         ]} />
 
         <FormItemSet classes="join flex justify-center items-center" items={[
-          <Controller name="password" control={control} render={({ field }) => [
-              <KeyRound className="ml-3 w-12" />,
-              <FormItem field={field} autocomplete="new-password" id="password" type="password" text="Jelszó" required={true} />
+          <Controller rules={{ required: true }} name="password" control={control} render={({ field }) => [
+            <KeyRound className="ml-3 w-12" />,
+            <FormItem field={field} autocomplete="new-password" id="password" type="password" text="Jelszó" />
           ]} />,
           <FormItem type="button" onclick={() => {
             setValue("password", generateRandomPassword(16));
@@ -55,16 +54,16 @@ export default function App() {
 
       <FormItemSet title="Életkor" items={[
         <div className="join gap-2">
-          <Controller name="agegroup" control={control} render={({ field }) => (
+          <Controller rules={{ required: true }} name="agegroup" control={control} render={({ field }) => (
             <FormItem field={field} type="radio" text="18-20" />
           )} />
-          <Controller name="agegroup" control={control} render={({ field }) => (
+          <Controller rules={{ required: true }} name="agegroup" control={control} render={({ field }) => (
             <FormItem field={field} type="radio" text="25-30" />
           )} />
-          <Controller name="agegroup" control={control} render={({ field }) => (
+          <Controller rules={{ required: true }} name="agegroup" control={control} render={({ field }) => (
             <FormItem field={field} type="radio" text="21-24" />
           )} />
-          <Controller name="agegroup" control={control} render={({ field }) => (
+          <Controller rules={{ required: true }} name="agegroup" control={control} render={({ field }) => (
             <FormItem field={field} type="radio" text="30+" />
           )} />
         </div>
@@ -72,51 +71,51 @@ export default function App() {
 
       <FormItemSet title="Nem" items={[
         <div className="join gap-10 flex justify-center items-center">
-          <Controller name="gender" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Férfi" required={true} />
+          <Controller rules={{ required: true }} name="gender" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Férfi" />
           )} />
-          <Controller name="gender" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Nő" required={true} />
+          <Controller rules={{ required: true }} name="gender" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Nő" />
           )} />
         </div>
       ]} />,
 
       <FormItemSet title="Ágazat" items={[
         <div className="grid grid-cols-2 gap-x-12">
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Bányászat" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Bányászat" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Egészségügy" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Egészségügy" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Építőipar" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Építőipar" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Élelmiszeripar" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Élelmiszeripar" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Épületgépészet" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Épületgépészet" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Honvédelem" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Honvédelem" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Gépészet" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Gépészet" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Kereskedelem" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Kereskedelem" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Oktatás" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Oktatás" />
           )} />
-          <Controller name="field" control={control} render={({ field }) => (
-            <FormItem field={field} type="radio" text="Szociális" required={true} />
+          <Controller rules={{ required: true }} name="field" control={control} render={({ field }) => (
+            <FormItem field={field} type="radio" text="Szociális" />
           )} />
         </div>
       ]} />,
 
-      <FormItem type="submit" classes="mt-2" text="Regisztráció" />,
+      <FormItem type="submit" onclick={() => console.log(errors)} classes="mt-2" text="Regisztráció" />,
     ]} />
   ]
 }
